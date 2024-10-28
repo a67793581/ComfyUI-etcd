9f2b59caf8ec3f27f9e600e8f566ad1ff6e3ab59
Carlo <284474102@qq.com>
2024-10-28 18:48:59 +0800

    init

--- __init__.py
+++ __init__.py
@@ -1,9 +1,24 @@
 import asyncio
+import threading
 
 from dotenv import load_dotenv
 load_dotenv()
 
 from src import Registered
-asyncio.run(Registered())
 
 
+def start_registered():
+    """
+    启动注册任务
+    """
+    asyncio.run(Registered())
+
+def start_registered_in_thread():
+    """
+    在单独的线程中启动注册任务
+    """
+    thread = threading.Thread(target=start_registered, daemon=True)
+    thread.start()
+
+# 在导入时自动启动任务
+start_registered_in_thread()
